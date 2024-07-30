97297654c24b5e3253ee27debd60859f2a4d55cd
MingFei2001 <mingfei2001@gmail.com>
2024-07-30 22:54:50 +0800

    added the main code

---
+++ ascii-timer.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> // For sleep function
+
+void print_ascii_art(const char *filename) {
+  FILE *file = fopen(filename, "r");
+  if (!file) {
+    perror("Could not open ASCII art file");
+    exit(EXIT_FAILURE);
+  }
+
+  char line[256];
+  while (fgets(line, sizeof(line), file)) {
+    printf("%s", line);
+  }
+
+  fclose(file);
+}
+
+void print_timer(int seconds) {
+  for (int i = seconds; i > 0; --i) {
+    printf("\\rTime left: %d seconds", i);
+    fflush(stdout);
+    sleep(1);
+  }
+  printf("\\nTime's up!\\n");
+}
+
+int main() {
+  int duration;
+  char ascii_art_file[256];
+
+  printf("Enter the duration (in seconds): ");
+  scanf("%d", &duration);
+  printf("Enter the ASCII art file path: ");
+  scanf("%s", ascii_art_file);
+
+  print_ascii_art(ascii_art_file);
+  print_timer(duration);
+
+  return 0;
+}
